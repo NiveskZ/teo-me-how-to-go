f3052cd5399f0812b71fff002bb7058f7ba80657
NiveskZ <krsmjogos@outlook.com>
2026-01-20 23:05:24 -0300

    feat: entendendo variaveis

---
+++ 03_variaveis/declaracao/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	fmt.Println("x =", x)
+
+	var y int = 10
+	fmt.Println("y =", y)
+
+	x = 45
+	fmt.Println("x =", x)
+
+	var z, w, v float64
+	fmt.Printf("z = %f | w = %f | v = %f \n", z, w, v)
+
+	var a, b, c string = "Kevin", "tentando", "go"
+	fmt.Println(a, b, c)
+}
---
+++ 03_variaveis/entrada/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Bora teclar? Me diz um valor")
+
+	var x float64
+	fmt.Scanf("%f", &x)
+
+	fmt.Println("Voce escolheu o valor", x)
+}
